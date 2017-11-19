f5b2381431be6b93691b3cc7b08c8e07c56e6149
Nguyễn Tuấn Anh <nguyentuananh11b6@gmail.com>
2017-11-19 10:09:28 +0700

    Merge branch 'master' of https://github.com/thuycuppy/itjob
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/ptit/itjob/WebConfiguration.java
    #	src/main/java/com/ptit/itjob/model/Candidate.java
    #	src/main/java/com/ptit/itjob/model/Job.java
    #	src/main/java/com/ptit/itjob/service/impl/ProfileServiceImpl.java
    #	src/main/resources/application.properties
    #	src/main/resources/static/css/slideshowstyle.css

--- src/test/java/com/ptit/itjob/ItjobApplicationTests.java
+++ src/test/java/com/ptit/itjob/ItjobApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ItJobApplicationTests {
+public class ITJobApplicationTests {
 
 	@Test
 	public void contextLoads() {
